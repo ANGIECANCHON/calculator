e345df6e0f1b6a7aa0abc4881b59dc5f8737e8e4
angie canchon <angie.canchon@rappi.com>
2020-07-30 00:34:33 -0500

    test automation for calculator

---
+++ src/test/java/Calculator.java
@@ -0,0 +1,58 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.*;
+import java.util.concurrent.TimeUnit;
+
+
+public class Calculator {
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() throws MalformedURLException{
+        //Set up desired capabilities and pass the Android app-activity and app-package to Appium
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("BROWSER_NAME", "Android");
+        capabilities.setCapability("VERSION", "4.4.2");
+        capabilities.setCapability("deviceName","Emulator");
+        capabilities.setCapability("platformName","Android");
+
+
+        capabilities.setCapability("appPackage", "com.android.calculator2");
+// This package name of your app (you can get it from apk info app)
+        capabilities.setCapability("appActivity","com.android.calculator2.Calculator"); // This is Launcher activity of your app (you can get it from apk info app)
+//Create RemoteWebDriver instance and connect to the Appium server
+        //It will launch the Calculator App in Android Device using the configurations specified in Desired Capabilities
+        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @Test
+    public void testCal() throws Exception {
+        //locate the Text on the calculator by using By.name()
+        WebElement two=driver.findElement(By.xpath("//android.widget.Button[@text='2']"));
+        two.click();
+        WebElement plus=driver.findElement(By.xpath("//android.widget.Button[@text='+']"));
+        plus.click();
+        WebElement four=driver.findElement(By.xpath("//android.widget.Button[@text='4']"));
+        four.click();
+        WebElement equalTo=driver.findElement(By.xpath("//android.widget.Button[@text='=']"));
+        equalTo.click();
+        //locate the edit box of the calculator by using By.tagName()
+        TimeUnit.SECONDS.sleep(1);
+        WebElement results=driver.findElement(By.xpath("//android.widget.TextView[@long-clickable='true']"));
+        //Check the calculated value on the edit box
+        assert results.getText().equals("6"):"Actual value is : "+results.getText()+" did not match with expected value: 6";
+
+    }
+
+    @AfterClass
+    public void teardown(){
+        //close the app
+        driver.quit();
+    }
+}
